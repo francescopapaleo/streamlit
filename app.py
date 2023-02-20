29cbbeb79fbf97f6d318d998d52f9b77e43cf0a1
Francesco Papaleo <francesco.papaleo@gmail.com>
2023-02-20 15:01:12 +0100

    test

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pandas as pd
 import altair as alt
 
 m3u_filepaths_file = 'playlists/streamlit.m3u8'
-ESSENTIA_ANALYSIS_PATH = '/Users/francescopapaleo/Dropbox/Mac/Documents/git-box/streamlit/data_test/descriptors_rel_path.json'
+ESSENTIA_ANALYSIS_PATH = '/data/descriptors_rel_path.json'
 
 
 def load_essentia_analysis():
