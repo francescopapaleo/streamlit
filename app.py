6723e576d331d339593432897e214abb27bc0ad9
Francesco Papaleo <francesco.papaleo@gmail.com>
2023-02-23 13:20:24 +0100

    app

--- app.py
+++ app.py
@@ -5,22 +5,29 @@ import random
 
 import streamlit as st
 import pandas as pd
+import ast
 
 CURRENT_DIR = Path().absolute()
 sys.path.append('./data')
 
 from style_labels import style_400_keys
 
-ESSENTIA_ANALYSIS_PATH = 'data/extended_descriptors_output_processed.json'
+ESSENTIA_ANALYSIS_PATH = 'data/extended_descriptors_reformat.json'
 
 def load_essentia_analysis():
-    return pd.read_json(ESSENTIA_ANALYSIS_PATH)
+    df = pd.read_json(ESSENTIA_ANALYSIS_PATH)
+    # Convert style_activations column from string to dictionary
+    df['style_activations'] = df['style_activations'].apply(lambda x: ast.literal_eval(x))
+    df.to_json('data/extended_descriptors_reformat.json', indent=4)
+    return df
 
-def filter_by_style(df, style_select):
-    style_select = ' '.join(style_select).lower().replace(' ', '_')
-    style_query = df.loc[(df['style_activations'].apply(lambda x: x[style_select] >= 0.5)).all(axis=1)]
+def filter_by_style(df, style_input):
+    style_query = df[['track_id', 'style_activations']].copy()
+    style_query['activation'] = style_query['style_activations'].apply(lambda x: x.get(style_input, -1))
+    style_query = style_query[style_query['activation'] >= 0.5]
+    style_query = style_query.sort_values(by='activation', ascending=False)
+    style_query = style_query.merge(df, on='track_id')
     return style_query
-
 
 def filter_by_bpm(df, bpm_range):
     if bpm_range:
@@ -49,41 +56,21 @@ audio_analysis = load_essentia_analysis()
 st.write(audio_analysis.describe())
 st.write('Loaded audio analysis for', len(audio_analysis), 'tracks.')
 
-style_select = st.multiselect('Select by style activations:', style_400_keys)
+st.write(audio_analysis['style_activations'].describe())
+
+style_input = st.selectbox('Select a style:', style_400_keys)
 bpm_range = st.slider('Select BPM range:', value=[int(audio_analysis['bpm'].min()), int(audio_analysis['bpm'].max())])
 danceability_range = st.slider('Select danceability range:', value=[0.0, 1.0])
 voice_instrumental_select = st.selectbox('Select voice/instrumental:', ['', 'instrumental', 'voice'])
-# valence_arousal_range = st.slider('Select valence/arousal range:', -1.0, 1.0, (-1.0, 1.0), 0.1)
+valence_arousal_range = st.slider('Select valence/arousal range:', -1.0, 1.0, (-1.0, 1.0), 0.1)
 
-df = filter_by_style(audio_analysis, style_select)
+style_query = filter_by_style(audio_analysis, style_select, track_idx)
+
+df = filter_by_style(audio_analysis, style_input)
 df = filter_by_bpm(df, bpm_range)
 df = filter_by_danceability(df, danceability_range)
 df = filter_by_voice_instrumental(df, voice_instrumental_select)
-# df = filter_by_valence_arousal(df, valence_arousal_range)
+df = filter_by_valence_arousal(df, valence_arousal_range)
+
 
 st.write(df)
-
-# max_tracks = st.number_input('Maximum number of tracks (0 for all):', value=0)
-# shuffle = st.checkbox('Random shuffle')
-
-# if st.button("RUN"):
-#         st.write('## 🔊 Results')
-#         mp3s = list(style_query.index)
-
-#         if shuffle:
-#             random.shuffle(mp3s)
-
-#         if max_tracks > 0:
-#             mp3s = mp3s[:max_tracks]
-
-#         with open(m3u_filepaths_file, 'w') as f:
-#             for mp3 in mp3s:
-#                 f.write(mp3 + '\n')
-
-#         st.write('Wrote', len(mp3s), 'tracks to', m3u_filepaths_file)
-
-#         with st.beta_expander("Results"):
-#             for mp3 in mp3s:
-#                 st.write(mp3)
-            
-#         st.audio(m3u_filepaths_file)
